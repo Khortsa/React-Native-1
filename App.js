3bf1c080eed3756ad8809a5eb7242e6b5ad1ce6f
Tenzin Choezom <khortsa2021@gmail.com>
2022-04-06 14:56:50 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import {FlatList, Text} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default function App(){
+  const [countries, setCountries] = useState([]);
+
+
+  useEffect(() =>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(
+      (result) =>{
+     setCountries(result);
+      }
+    )
+  },[],)
+  
+  return(
+    // <FlatList>
+      <Text>Buzzer</Text>
+    // </FlatList>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
